489565b7cfcace88d702ffc75f5d95559382493d
hotire <gngh0101@gmail.com>
2020-05-27 01:48:02 +0900

    Add comment

--- src/test/java/com/github/hotire/map/struct/SimpleSourceDestinationMapperTest.java
+++ src/test/java/com/github/hotire/map/struct/SimpleSourceDestinationMapperTest.java
@@ -9,26 +9,35 @@ import org.mapstruct.factory.Mappers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SimpleSourceDestinationMapperTest {
-    private SimpleSourceDestinationMapper mapper
+    private final SimpleSourceDestinationMapper mapper
             = Mappers.getMapper(SimpleSourceDestinationMapper.class);
 
     @Test
     void givenSourceToDestination_whenMaps_thenCorrect() {
-        SimpleSource simpleSource = new SimpleSource();
+        // given
+        final SimpleSource simpleSource = new SimpleSource();
         simpleSource.setName("SourceName");
         simpleSource.setDescription("SourceDescription");
-        SimpleDestination destination = mapper.sourceToDestination(simpleSource);
 
+        // when
+        final SimpleDestination destination = mapper.sourceToDestination(simpleSource);
+
+        // then
         assertEquals(simpleSource.getName(), destination.getName());
         assertEquals(simpleSource.getDescription(),
                 destination.getDescription());
     }
     @Test
     void givenDestinationToSource_whenMaps_thenCorrect() {
-        SimpleDestination destination = new SimpleDestination();
+        // given
+        final SimpleDestination destination = new SimpleDestination();
         destination.setName("DestinationName");
         destination.setDescription("DestinationDescription");
-        SimpleSource source = mapper.destinationToSource(destination);
+
+        // when
+        final SimpleSource source = mapper.destinationToSource(destination);
+
+        // then
         assertEquals(destination.getName(), source.getName());
         assertEquals(destination.getDescription(),
                 source.getDescription());
