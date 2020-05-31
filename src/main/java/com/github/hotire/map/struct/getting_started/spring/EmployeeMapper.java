3fdbe9db8f2727dd0ff154b2417f4767a5f19b47
hotire <gngh0101@gmail.com>
2020-06-01 01:20:10 +0900

    Fix EmployeeMapper

--- src/main/java/com/github/hotire/map/struct/getting_started/spring/EmployeeMapper.java
+++ src/main/java/com/github/hotire/map/struct/getting_started/spring/EmployeeMapper.java
@@ -1,7 +1,20 @@
 package com.github.hotire.map.struct.getting_started.spring;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
+    @Mappings({
+            @Mapping(target = "employeeId", source = "entity.id"),
+            @Mapping(target = "employeeName", source = "entity.name")
+    })
+    V1Employee employeeToV1(final Employee entity);
+
+    @Mappings({
+            @Mapping(target = "id", source = "v1.employeeId"),
+            @Mapping(target = "name", source = "v1.employeeName")
+    })
+    Employee v1ToEmployee(final V1Employee v1);
 }
