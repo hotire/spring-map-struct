99c4f8608382bd9ca21cfebd106fcfe275f345f5
hotire <gngh0101@gmail.com>
2020-06-01 01:29:19 +0900

    Add Division enttiy

---
+++ src/main/java/com/github/hotire/map/struct/getting_started/spring/Division.java
@@ -0,0 +1,9 @@
+package com.github.hotire.map.struct.getting_started.spring;
+
+import lombok.Data;
+
+@Data
+public class Division {
+    private Integer id;
+    private String name;
+}
---
+++ src/main/java/com/github/hotire/map/struct/getting_started/spring/V1Division.java
@@ -0,0 +1,9 @@
+package com.github.hotire.map.struct.getting_started.spring;
+
+import lombok.Data;
+
+@Data
+public class V1Division {
+    private Integer id;
+    private String name;
+}
