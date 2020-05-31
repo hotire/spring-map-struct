994a8832474e6f510a4ee8e750864e04f82b6fd5
hotire <gngh0101@gmail.com>
2020-06-01 01:20:45 +0900

    Verify v1ToEmployee

---
+++ src/test/java/com/github/hotire/map/struct/getting_started/spring/EmployeeMapperTest.java
@@ -0,0 +1,26 @@
+package com.github.hotire.map.struct.getting_started.spring;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EmployeeMapperTest {
+
+    private final EmployeeMapper mapper
+            = Mappers.getMapper(EmployeeMapper.class);
+    @Test
+    void v1ToEmployee() {
+        // given
+        final V1Employee v1Employee = new V1Employee();
+        v1Employee.setEmployeeId(1);
+        v1Employee.setEmployeeName("hotire");
+
+        // when
+        final Employee result = mapper.v1ToEmployee(v1Employee);
+
+        // then
+        assertThat(result.getId()).isEqualTo(v1Employee.getEmployeeId());
+        assertThat(result.getName()).isEqualTo(v1Employee.getEmployeeName());
+    }
+}
