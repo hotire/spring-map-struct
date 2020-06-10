3767ae23150d5c262d735dc5833b80b9a4f65e75
hotire <gngh0101@gmail.com>
2020-06-11 00:57:59 +0900

    Add Accessors

--- src/main/java/com/github/hotire/map/struct/getting_started/before_after/Car.java
+++ src/main/java/com/github/hotire/map/struct/getting_started/before_after/Car.java
@@ -1,8 +1,10 @@
 package com.github.hotire.map.struct.getting_started.before_after;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public abstract class Car implements CarTypeAware {
     private Long id;
     private String name;
